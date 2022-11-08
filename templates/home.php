2765a1531e6d6ce53c5008a769d4bb2dea9ea993
applh.com <contact@applh.com>
2022-11-09 00:32:37 +0100

    v0.1.1 add basic css

--- templates/home.php
+++ templates/home.php
@@ -6,6 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="GAIA">
     <title>GAIA</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+            font-size: 16px;
+        }
+        body * {
+            box-sizing: border-box;
+        }
+        
+        h1, h2, h3 {
+            text-align: center;
+        }
+        a {
+            text-decoration: none;
+            color: #000;
+        }
+    </style>
 </head>
 <body>
     <header>
