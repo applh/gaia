1ae0a6a5495598a5efafe63e34dca0f58cca9f0e
applh.com <contact@applh.com>
2022-11-10 02:35:38 +0100

    v0.2.0 add slide title font and background image

--- class/movie.php
+++ class/movie.php
@@ -59,63 +59,118 @@ class movie
 
         // load font
         $path_media = gaia::kv("path_media");
+
         $font = "$path_media/fonts/RobotoMono-Light.ttf";
+        $font_title = "$path_media/fonts/Monoton-Regular.ttf";
+        gaia::kv("movie/font", $font);
+        gaia::kv("movie/font_title", $font_title);
+        gaia::kv("movie/font_size", $font_size);
+
         // loop and build frames
         foreach(range(0, $nb_frames-1) as $frame_index)
         {
             
             // get the frame file path
-            $frame_file = "$path_movies/f-$frame_index.png";
+            $frame_file = "$path_movies/f-$frame_index.webp";
 
             // debug
             echo "frame($title,$frame_index) $frame_file".PHP_EOL;
 
-            // create the frame
-            $frame = imagecreatetruecolor($width, $height);
-            // fill the frame with a color
-            $transparent = imagecolorallocatealpha($frame, 255, 255, 255, 127);
-            imagefill($frame, 0, 0, $transparent);
+
             // write the frame index
-            $black = imagecolorallocatealpha($frame, 0, 0, 0, 0);
-            $red = imagecolorallocatealpha($frame, 255, 0, 0, 0);
             // write text with ttf font
             $slide_title = $slides[$frame_index]["title"] ?? $title ?? "";
             $slide_content = $slides[$frame_index]["content"] ?? "";
 
+            // trim title
+            $slide_title = trim($slide_title, "# ");
+
             $page_number = $frame_index+1;
 
             $text = <<<txt
-            
-            $slide_title
-            
-            $slide_content
 
-            ---------*---------*---------*---------*---------*
-            $page_number
+            $slide_content
 
             txt;
 
+            // ---------*---------*---------*---------*---------*
 
-            movie::text_center($frame, $text, $black, $font_size, $font, $width, $height);
+            movie::text_center($frame_file, $slide_title, $text, $page_number, $width, $height);
 
-            // save alpha channel
-            imagesavealpha($frame, true);
-            // save the frame
-            // WARNING: webp frames are bad for ffmpeg
-            imagepng($frame, $frame_file);
-            // free the frame
-            imagedestroy($frame);
         }
+
+        // destroy earth image
+        $earth = gaia::kv("movie/earth");
+        if ($earth) imagedestroy($earth);
+
         return $path_movies;
     }
 
-    static function text_center ($frame, $text, $color, $font_size, $font, $width, $height)
+    static function text_center ($frame_file, $title, $text, $footer, $width, $height)
     {
-        $text_width = imagettfbbox($font_size, 0, $font, $text)[2];
-        $text_height = imagettfbbox($font_size, 0, $font, $text)[3];
-        $text_x = round(0.5 * ($width - $text_width));
-        $text_y = round(0.5 * ($height - $text_height));
-        imagettftext($frame, $font_size, 0, $text_x, $text_y, $color, $font, $text);
+        static $earth = null;
+        if (!$earth) {
+            // fill the frame with image pages/earth-night.jpg
+            $path_data = gaia::kv("path_data");
+            $earth_path = "$path_data/pages/earth-night.jpg";
+            $earth = imagecreatefromjpeg($earth_path);
+
+            gaia::kv("movie/earth", $earth);
+        }
+
+        // create the frame
+        $frame = imagecreatetruecolor($width, $height);
+
+        $transparent = imagecolorallocatealpha($frame, 255, 255, 255, 127);
+        $trans100 = imagecolorallocatealpha($frame, 0, 0, 0, 100);
+        $black = imagecolorallocatealpha($frame, 0, 0, 0, 0);
+        $white = imagecolorallocatealpha($frame, 255, 255, 255, 0);
+        $red = imagecolorallocatealpha($frame, 255, 0, 0, 0);
+        
+        // fill the frame with a color
+        // imagefill($frame, 0, 0, $transparent);
+        // imagefill($frame, 0, 0, $black);
+
+        // imagecopy($frame, $earth, 0, 0, 0, 0, 1680, 2160);
+        // image copy resampled with full width 
+        if ($earth) {
+            $earth_w = imagesx($earth);
+            $earth_h = imagesy($earth);
+            $earth_ratio = $earth_h / $earth_w;
+            $dst_h = $width * $earth_ratio;
+
+            imagecopyresampled($frame, $earth, 0, 0, 0, 0, $width, $dst_h, $earth_w, $earth_h);    
+            imagefill($frame, 0, 0, $trans100);
+        }
+
+        $font = gaia::kv("movie/font");
+        $font_title = gaia::kv("movie/font_title");
+        $font_size = gaia::kv("movie/font_size");
+        $width = imagesx($frame);
+        $height = imagesy($frame);
+
+        $points = imagettfbbox($font_size, 0, $font, $text);
+        $text_width = $points[2];
+        $text_height = $points[3];
+        // $text_x = round(0.5 * ($width - $text_width));
+        // $text_y = round(0.5 * ($height - $text_height));
+        
+        $text_x = 2 * $font_size;
+
+        // title
+        imagettftext($frame, $font_size+10, 0, $text_x, 4*$font_size, $red, $font_title, $title);
+        // content
+        imagettftext($frame, $font_size, 0, $text_x, 6*$font_size, $white, $font, $text);
+        // footer
+        imagettftext($frame, $font_size-10, 0, $text_x, $height-2*$font_size, $white, $font, $footer);
+
+        // save alpha channel
+        imagesavealpha($frame, true);
+        // save the frame
+        // WARNING: webp frames are bad for ffmpeg
+        imagewebp($frame, $frame_file);
+        // free the frame
+        imagedestroy($frame);
     }
 
     static function build_movie ()
@@ -138,8 +193,9 @@ class movie
         // call ffmpeg to build the movie
         $path_data = gaia::kv("path_data");
         $now = date("ymd-His");
-        $path_movie = "$path_data/movies/$file-$now.mp4";
-        $cmd = "ffmpeg -framerate 1/6 -i $path_frames/f-%d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p $path_movie";
+        $path_movie = "$path_data/movies/$file-$now.webm";
+        // $cmd = "ffmpeg -framerate 1/6 -i $path_frames/f-%d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p $path_movie";
+        $cmd = "ffmpeg -framerate 1/6 -i $path_frames/f-%d.webp -crf 20 $path_movie";
         shell_exec($cmd);
     }
 
