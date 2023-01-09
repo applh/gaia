62271bee8011e60c2d8d9fa238ca2b3bbd0ddbda
AppLH <hello@applh.com>
2023-01-09 21:23:37 +0000

    v0.7.0 mix uikit+vue+leaflet

--- templates/uikit.php
+++ templates/uikit.php
@@ -75,7 +75,64 @@ $now = date('Y-m-d H:i:s');
     <script src="/media/uikit/uikit.min.js"></script>
     <script src="/media/uikit/uikit-icons.min.js"></script>
     <script src="/media/leaflet/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
-    <script type="module" src="/media/site.js"></script>
+    <script type="module">
+console.log('site.js loaded');
+
+// import vuesJS from CDN and create Vue app
+import * as Vue from 'https://cdn.jsdelivr.net/npm/vue@3.2.45/dist/vue.esm-browser.prod.js';
+const app = Vue.createApp({
+    template: '#appTemplate',
+    mounted () {
+        const map = L.map('map').setView([51.505, -0.09], 13);
+
+        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    
+        const marker = L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+    
+        const circle = L.circle([51.508, -0.11], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 500
+        }).addTo(map).bindPopup('I am a circle.');
+    
+        const polygon = L.polygon([
+            [51.509, -0.08],
+            [51.503, -0.06],
+            [51.51, -0.047]
+        ]).addTo(map).bindPopup('I am a polygon.');
+    
+    
+        const popup = L.popup()
+            .setLatLng([51.513, -0.09])
+            .setContent('I am a standalone popup.')
+            .openOn(map);
+    
+        function onMapClick(e) {
+            popup
+                .setLatLng(e.latlng)
+                .setContent(`You clicked the map at ${e.latlng.toString()}`)
+                .openOn(map);
+        }
+    
+        map.on('click', onMapClick);
+    },
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+});
+// mount Vue app
+app.mount('#app');
+
+
+
+    </script>
 </body>
 
 </html>
